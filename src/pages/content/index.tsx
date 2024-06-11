b0f9a7044e9de7c8bf1ebc2074cc71fee59f3b56
Toan Vo <vothaitoan12@gmail.com>
2024-06-11 11:41:55 +0700

    feat: add sync state for web page with toggle button worked

--- src/pages/content/index.tsx
+++ src/pages/content/index.tsx
@@ -1,11 +1,11 @@
 import ReactDOMServer from "react-dom/server";
 import "./style.css";
 import Block from "../block";
-const div = document.createElement("div");
-div.id = "__root";
-document.body.appendChild(div);
-const rootContainer = document.querySelector("#__root");
-if (!rootContainer) throw new Error("Can't find Content root element");
+// const div = document.createElement("div");
+// div.id = "__root";
+// document.body.appendChild(div);
+// const rootContainer = document.querySelector("#__root");
+// if (!rootContainer) throw new Error("Can't find Content root element");
 // const root = createRoot(rootContainer);
 let bannedPage: string[] = [];
 chrome.storage.sync.get(["allPages"], function (result) {
--- src/pages/newtab/Newtab.tsx
+++ src/pages/newtab/Newtab.tsx
@@ -1,14 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "@pages/newtab/Newtab.css";
 import worker from "@assets/img/worker.png";
 import Card from "./components/Card";
 
-export default function Newtab(): JSX.Element {
+export default function Newtab({ data }: { data: string[] }): JSX.Element {
+  const [initactivepage, setInitActivePage] = useState<string[]>(data);
+
+  useEffect(() => {
+    // Initialize activePage from storage
+    console.log("reload data", initactivepage);
+    chrome.storage.sync.set({ allPages: initactivepage }, function () {
+      console.log("Data saved");
+    });
+  }, [initactivepage]);
   return (
     <div className="flex h-screen bg-white">
       <div className="m-auto shadow-lg p-6 flex flex-col items-center justify-center gap-2 rounded-xl border-t border-gray-300">
         <img src={worker} alt="sample" width={200} height={200} />
-        <Card />
+        <Card
+          name="LinkedIn"
+          url="www.linkedin.com"
+          allActivePage={initactivepage}
+          setInitActivePage={setInitActivePage}
+        />
+        <Card
+          name="Youtube"
+          url="www.youtube.com"
+          allActivePage={initactivepage}
+          setInitActivePage={setInitActivePage}
+        />
+        <Card
+          name="Facebook"
+          url="www.facebook.com"
+          allActivePage={initactivepage}
+          setInitActivePage={setInitActivePage}
+        />
+        <Card
+          name="Messenger"
+          url="www.messenger.com"
+          allActivePage={initactivepage}
+          setInitActivePage={setInitActivePage}
+        />
+        <Card
+          name="Pinterest"
+          url="www.pinterest.com"
+          allActivePage={initactivepage}
+          setInitActivePage={setInitActivePage}
+        />
+        <Card
+          name="Twitter"
+          url="www.twitter.com"
+          allActivePage={initactivepage}
+          setInitActivePage={setInitActivePage}
+        />
       </div>
     </div>
   );
